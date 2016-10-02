acce4718349c34687f63541bb5dd2c14f8e0d742
princessRuto <cindy.lorraine.g@gmail.com>
2016-10-01 23:15:25 -0500

    added vignere

--- encrypt.c
+++ encrypt.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <ctype.h>
 
-void caesar(int key, char * p)
+void encCaesar(int key, char * p)
 {
     char enc [100];
     for(int i=0; i < strlen(p); i++)
@@ -13,16 +13,40 @@ void caesar(int key, char * p)
     
 }
 
-void caesarDecrypt(int key, char * p){
+void decCaesar(int key, char * p){
     char dec [100];
     for(int i=0; i < strlen(p); i++)
         dec[i]=("%c", (((int)p[i] - 71 - key) %26) + 'a');
     printf("%s \n", dec);
 }
 
+void encVignere (char * key, char * p){
+    int count, countkey;
+    char result [100];
+    for(int i=0; i < strlen(p); i++){
+        count = (int)p[i] - 'a';
+        countkey = (int)key[i % strlen(key)] - 'a';
+        result[i] = ("%c", ((count + countkey) % 26) + 'a');
+    }
+    result[strlen(p)]=0;
+    printf("%s\n",result);
+}
+
+void decVignere (char * key, char * p){
+    int count, countkey;
+    char result [100];
+    for(int i=0; i < strlen(p); i++){
+        count = (int)p[i] - 71;
+        countkey = (int)key[i % strlen(key)] - 'a';
+        result[i] = ("%c", ((count - countkey) % 26) + 'a');
+    }
+    result[strlen(p)]=0;
+    printf("%s\n",result);
+}
+
 int main()
 {
 char *p = "hola";
-caesar(3, p);
+encCaesar(3, p);
 }
 
