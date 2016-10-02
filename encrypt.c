3b6f27b60df2a7cba8ab5232d55d7e628de3c01d
princessRuto <cindy.lorraine.g@gmail.com>
2016-10-01 22:35:00 -0500

    fixed this crap

--- encrypt.c
+++ encrypt.c
@@ -2,34 +2,28 @@
 #include <string.h>
 #include <ctype.h>
 
-void caesar(int key)
+void caesar(int key, char * p)
 {
-    char p[100];
-    int i,enc;
-    if(key>26)
-      key=key % 26;
-      
-    printf("Enter the sentence\n");
-    while((i=getchar()) != '\n')
-    {
-           p[i]=tolower(p[i]);
-           enc=((int)p[i] - 97 + key) % 26;
-           printf("%c",enc + 97);
-    }
+   // int i;
+    char enc [100];
+    for(int i=0; i < strlen(p); i++)
+        enc[i]=("%c" , (((int)p[i] - 'a' + key) % 26) + 'a');
+    enc[strlen(p)] = 0;
+    printf("%s \n", enc);
     
 }
 
 void caesarDecrypt(int key, char * p){
     int i;
     char dec [100];
-    for(i=0; i =< strlen(p); i++)
+    for(i=0; i < strlen(p); i++)
         dec[i]=("%c", (((int)p[i] - 71 - key) %26) + 97);
     printf("%s \n", dec);
 }
 
 int main()
 {
-char *p = "krod";
-caesarDecrypt(3,p);
+char *p = "hola";
+caesar(3, p);
 }
 
